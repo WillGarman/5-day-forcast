0d84bef69ecb40806ed7e2c9277bac842a52bf55
WillGarman <wgarman702@gmail.com>
2022-05-23 02:56:42 -0400

    update

--- past/UITest.py
+++ past/UITest.py
@@ -4,14 +4,19 @@
 #https://www.weatherbit.io/api/weather-forecast-hourly
 #https://www.weatherbit.io/api/swaggerui/weather-api-v2#!/24032hour324732hourly32Forecast/get_forecast_hourly_city_city_country_country
 
+import sys
 import os
 import requests
 import json
+from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton, QMenuBar, QTextBrowser, QWidget, QComboBox
+from PyQt5 import uic
+
 
 API_KEY = '05a59ce96cb307a4e12ed503a455c1a0'
 
 def get_lat_lon(country, state, city):
     location_r = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city},{state},{country}&limit=1&appid={API_KEY}')
+
     if(location_r.ok):
 
         location_info = json.loads(location_r.text)
